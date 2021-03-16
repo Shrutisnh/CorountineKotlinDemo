0374f78609bff06033042df535d2e3d03396bed7
Shruti <shruti_sinha@persistent.com>
2021-03-16 16:41:55 +0530

    Coroutine jobs- Completeable

--- app/src/main/java/com/example/corountinekotlindemo/MainActivity.kt
+++ app/src/main/java/com/example/corountinekotlindemo/MainActivity.kt
@@ -1,68 +1,97 @@
 package com.example.coroutineexample
 
 import android.os.Bundle
+import android.widget.ProgressBar
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.corountinekotlindemo.R
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.*
 import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.Dispatchers.Main
+import kotlin.coroutines.cancellation.CancellationException
 
 
 class MainActivity : AppCompatActivity(){
-    private val RESULT_1 = "RESULT#1"
-    private val RESULT_2 = "RESULT#2"
 
-    val JOB_TIMEOUT = 2100L
+    private val PROGRESS_MAX = 100
+    private val PROGRESS_START = 0
+    private val JOB_TIME = 4000
+    private lateinit var job: CompletableJob
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        button.setOnClickListener {
-            CoroutineScope(IO).launch {
-                fakeApiRequest()
+        job_button.setOnClickListener {
+            if(!::job.isInitialized){
+                initJob()
+            }
+            job_progress_bar.startJobOrCancel(job)
+        }
+    }
+
+    fun ProgressBar.startJobOrCancel(job: Job){
+        if(this.progress>0) {
+            println("This job has already been started")
+            resetJob(job)
+        }
+        else {
+            job_button.setText("Cancel Job#1")
+
+            CoroutineScope(IO + job).launch {
+                println("coroutine ${this} is activated with job ${job}")
+                println("thread: ${Thread.currentThread().name}")
+
+                for(i in PROGRESS_START..PROGRESS_MAX) {
+                    delay((JOB_TIME / PROGRESS_MAX).toLong())
+                    this@startJobOrCancel.progress = i
+                }
+                updateJobCompleteTextView("Job is Complete")
+
+            }
+
+        }
+    }
+
+    private fun resetJob(job: Job){
+        if(job.isActive|| job.isCompleted)
+            job.cancel(CancellationException("Resetting Job"))
+    initJob()
+    }
+
+
+    fun initJob(){
+        job_button.setText("Start Job#1")
+        job_complete_text.setText("")
+        job = Job()
+        job.invokeOnCompletion {
+            it?.message.let {
+                var msg =it
+                if(msg.isNullOrBlank()){
+                    println("Unknown Cancellation error")
+                }else {
+                    println("$job was cancelled because: $msg")
+                }
+                msg?.let { it1 -> showToast(it1) }
             }
         }
+
+        job_progress_bar.max = PROGRESS_MAX
+        job_progress_bar.progress = PROGRESS_START
+
     }
 
-    private suspend fun setTextOnMainThread(text:String){
-        withContext(Main){
-            textView.text = textView.text.toString()+"\n"+text
+    private fun updateJobCompleteTextView(text:String){
+        GlobalScope.launch(Main) {
+            job_complete_text.text = text
         }
     }
 
-    private suspend fun fakeApiRequest(){
-        val job = withContext(IO){
-            withTimeoutOrNull(JOB_TIMEOUT) {
-
-                val result1= getResult1fromApi()
-                println("debug: $result1")
-                setTextOnMainThread(result1)
-                val result2 = getResult2FromApi()
-                setTextOnMainThread(result2)
-                println("debug: $result2")
-            }
-
+    fun showToast(text: String){
+        GlobalScope.launch(Main) {
+            Toast.makeText(this@MainActivity,text,Toast.LENGTH_LONG).show()
         }
-        if (job==null){
-            println("debug: TIMEOUT")
-        }
-
     }
 
-    private suspend fun getResult1fromApi():String{
-        logThread("getResult1FromApi")
-        delay(1000)
-        return RESULT_1
-    }
-
-    private suspend fun getResult2FromApi(): String{
-        logThread("getResult2FromApi")
-        delay(1000)
-        return RESULT_2
-    }
-
-    private fun logThread(methodName:String){
-        println("debug: ${methodName}: ${Thread.currentThread().name}")
-    }
 }
