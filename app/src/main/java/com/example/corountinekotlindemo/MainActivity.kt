73963f718980142693931f4bdb9af317d826ca7f
Shruti <shruti_sinha@persistent.com>
2021-03-18 16:30:31 +0530

    Supervisor Jobs

--- app/src/main/java/com/example/corountinekotlindemo/MainActivity.kt
+++ app/src/main/java/com/example/corountinekotlindemo/MainActivity.kt
@@ -9,7 +9,7 @@ import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.Dispatchers.Main
 
 
-class MainActivity : AppCompatActivity(){
+class MainActivity : AppCompatActivity() {
     private val TAG: String = "AppDebug"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,66 +22,54 @@ class MainActivity : AppCompatActivity(){
         println("Exception thrown in one of the children: $exception")
     }
 
-    fun main(){
-        val parentJob = CoroutineScope(IO).launch(handler) {
-
-            // --------- JOB A ---------
-            val jobA = launch {
-                val resultA = getResult(1)
-                println("resultA: ${resultA}")
-            }
-            jobA.invokeOnCompletion { throwable ->
-                if(throwable != null){
-                    println("Error getting resultA: ${throwable}")
+    fun main() {
+        val parentJob = CoroutineScope(IO).launch (handler){
+            supervisorScope {
+                val jobA = launch {
+                    val resultA = getResult(1)
+                    println("resultA: ${resultA}")
                 }
-            }
 
-            // --------- JOB B ---------
-            val jobB = launch {
-                val resultB = getResult(2)
-                println("resultB: ${resultB}")
-            }
-            jobB.invokeOnCompletion { throwable ->
-                if(throwable != null){
-                    println("Error getting resultB: ${throwable}")
+                // --------- JOB B ---------
+                val jobB = launch {
+
+                    val resultB = getResult(2)
+                    println("resultB: ${resultB}")
                 }
+
+                // --------- JOB C ---------
+                val jobC = launch {
+                    val resultC = getResult(3)
+                    println("resultC: ${resultC}")
+                }
+
             }
 
-            // --------- JOB C ---------
-            val jobC = launch {
-                val resultC = getResult(3)
-                println("resultC: ${resultC}")
-            }
-            jobC.invokeOnCompletion { throwable ->
-                if(throwable != null){
-                    println("Error getting resultC: ${throwable}")
-                }
-            }
+
         }
         parentJob.invokeOnCompletion { throwable ->
-            if(throwable != null){
+            if (throwable != null) {
                 println("Parent job failed: ${throwable}")
-            }
-            else{
+            } else {
                 println("Parent job SUCCESS")
             }
         }
     }
 
-    suspend fun getResult(number: Int): Int{
-        return withContext(Main){
-            delay(number*500L)
-            if(number == 2){
+    suspend fun getResult(number: Int): Int {
+        return withContext(Main) {
+            delay(number * 500L)
+            if (number == 2) {
 //                cancel(CancellationException("Error getting result for number: ${number}"))
 //                throw CancellationException("Error getting result for number: ${number}") // treated like "cancel()"
                 throw Exception("Error getting result for number: ${number}")
             }
-            number*2
+            number * 2
         }
     }
 
 
-    private fun println(message: String){
+    private fun println(message: String) {
         Log.d(TAG, message)
     }
 }
