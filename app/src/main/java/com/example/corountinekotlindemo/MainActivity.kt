b6282678841edb32f32bbe7ebb66ca01db8e8346
Shruti <shruti_sinha@persistent.com>
2021-03-18 15:22:52 +0530

    Error handling and Exception handling

--- app/src/main/java/com/example/corountinekotlindemo/MainActivity.kt
+++ app/src/main/java/com/example/corountinekotlindemo/MainActivity.kt
@@ -1,68 +1,87 @@
 package com.example.coroutineexample
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.example.corountinekotlindemo.R
-import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.*
 import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.Dispatchers.Main
 
 
 class MainActivity : AppCompatActivity(){
-    private val RESULT_1 = "RESULT#1"
-    private val RESULT_2 = "RESULT#2"
-
-    val JOB_TIMEOUT = 2100L
+    private val TAG: String = "AppDebug"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        button.setOnClickListener {
-            CoroutineScope(IO).launch {
-                fakeApiRequest()
+        main()
+    }
+
+    val handler = CoroutineExceptionHandler { _, exception ->
+        println("Exception thrown in one of the children: $exception")
+    }
+
+    fun main(){
+        val parentJob = CoroutineScope(IO).launch(handler) {
+
+            // --------- JOB A ---------
+            val jobA = launch {
+                val resultA = getResult(1)
+                println("resultA: ${resultA}")
+            }
+            jobA.invokeOnCompletion { throwable ->
+                if(throwable != null){
+                    println("Error getting resultA: ${throwable}")
+                }
+            }
+
+            // --------- JOB B ---------
+            val jobB = launch {
+                val resultB = getResult(2)
+                println("resultB: ${resultB}")
+            }
+            jobB.invokeOnCompletion { throwable ->
+                if(throwable != null){
+                    println("Error getting resultB: ${throwable}")
+                }
+            }
+
+            // --------- JOB C ---------
+            val jobC = launch {
+                val resultC = getResult(3)
+                println("resultC: ${resultC}")
+            }
+            jobC.invokeOnCompletion { throwable ->
+                if(throwable != null){
+                    println("Error getting resultC: ${throwable}")
+                }
+            }
+        }
+        parentJob.invokeOnCompletion { throwable ->
+            if(throwable != null){
+                println("Parent job failed: ${throwable}")
+            }
+            else{
+                println("Parent job SUCCESS")
             }
         }
     }
 
-    private suspend fun setTextOnMainThread(text:String){
-        withContext(Main){
-            textView.text = textView.text.toString()+"\n"+text
-        }
-    }
-
-    private suspend fun fakeApiRequest(){
-        val job = withContext(IO){
-            withTimeoutOrNull(JOB_TIMEOUT) {
-
-                val result1= getResult1fromApi()
-                println("debug: $result1")
-                setTextOnMainThread(result1)
-                val result2 = getResult2FromApi()
-                setTextOnMainThread(result2)
-                println("debug: $result2")
+    suspend fun getResult(number: Int): Int{
+        return withContext(Main){
+            delay(number*500L)
+            if(number == 2){
+//                cancel(CancellationException("Error getting result for number: ${number}"))
+//                throw CancellationException("Error getting result for number: ${number}") // treated like "cancel()"
+                throw Exception("Error getting result for number: ${number}")
             }
-
+            number*2
         }
-        if (job==null){
-            println("debug: TIMEOUT")
-        }
-
     }
 
-    private suspend fun getResult1fromApi():String{
-        logThread("getResult1FromApi")
-        delay(1000)
-        return RESULT_1
-    }
 
-    private suspend fun getResult2FromApi(): String{
-        logThread("getResult2FromApi")
-        delay(1000)
-        return RESULT_2
-    }
-
-    private fun logThread(methodName:String){
-        println("debug: ${methodName}: ${Thread.currentThread().name}")
+    private fun println(message: String){
+        Log.d(TAG, message)
     }
 }
